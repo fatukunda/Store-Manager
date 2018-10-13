6b38e947e39b408ab0bd92d517d8ca7058beda06
Frank <fatukunda@gmail.com>
2018-10-13 15:14:51 +0300

    <Finish: 161108765> Add Product Category page

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -17,8 +17,10 @@ const toggle = (elem) => {
 }
 const productsCard = document.getElementById('products-card');
 const adminProductsList = document.getElementsByClassName('admin-products-list')[0];
+const addCategory = document.getElementById('add-category');
 const addProduct = document.getElementById('add-product');
 const addProductLink = document.getElementById('add-product-link');
+const addCategoryLink = document.getElementById('add-category-link');
 const viewProducts = document.getElementById('view-products');
 //Hide admin products list when the page loads
 window.addEventListener('load', () => {
@@ -29,13 +31,21 @@ window.addEventListener('load', () => {
 //Toggle admin products when the products card is clicked on
 productsCard.addEventListener('click', () => {
     show(adminProductsList);
+    hide(addCategory);
     hide(addProduct);
 });
 addProductLink.addEventListener('click', () => {
     hide(adminProductsList);
+    hide(addCategory);
     show(addProduct);
 });
+addCategoryLink.addEventListener('click', () => {
+    show(addCategory);
+    hide(adminProductsList);
+    hide(addProduct);
+})
 viewProducts.addEventListener('click', () => {
     hide(addProduct);
     show(adminProductsList);
+    hide(addCategory);
 })
