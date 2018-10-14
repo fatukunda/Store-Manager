27a13d4cd9c2a4ed2fb909c216bacdb140726e16
Frank <fatukunda@gmail.com>
2018-10-14 09:04:47 +0300

    <Finish: 161108793> View sales records page completed

--- UI/js/admin.js
+++ UI/js/admin.js
@@ -16,16 +16,21 @@ const toggle = (elem) => {
     show(elem);
 }
 const productsCard = document.getElementById('products-card');
+const salesCard = document.getElementById('sales-card');
 const adminProductsList = document.getElementsByClassName('admin-products-list')[0];
 const addCategory = document.getElementById('add-category');
 const addProduct = document.getElementById('add-product');
 const addProductLink = document.getElementById('add-product-link');
 const addCategoryLink = document.getElementById('add-category-link');
 const viewProducts = document.getElementById('view-products');
+const viewSales = document.getElementById('view-sales');
+const viewSalesLink = document.getElementById('view-sales-link')
 //Hide admin products list when the page loads
 window.addEventListener('load', () => {
-    hide(adminProductsList);
+    show(adminProductsList);
     hide(addProduct);
+    hide(viewSales);
+    hide(addCategory);
 
 });
 //Toggle admin products when the products card is clicked on
@@ -33,19 +38,35 @@ productsCard.addEventListener('click', () => {
     show(adminProductsList);
     hide(addCategory);
     hide(addProduct);
+    hide(viewSales);
 });
+salesCard.addEventListener('click', () => {
+    show(viewSales);
+    hide(adminProductsList);
+    hide(addCategory);
+    hide(addProduct);
+})
 addProductLink.addEventListener('click', () => {
     hide(adminProductsList);
     hide(addCategory);
     show(addProduct);
+    hide(viewSales);
 });
 addCategoryLink.addEventListener('click', () => {
     show(addCategory);
     hide(adminProductsList);
     hide(addProduct);
+    hide(viewSales);
 })
 viewProducts.addEventListener('click', () => {
     hide(addProduct);
     show(adminProductsList);
     hide(addCategory);
+    hide(viewSales);
+});
+viewSalesLink.addEventListener('click', () => {
+    show(viewSales);
+    hide(adminProductsList);
+    hide(addCategory);
+    hide(addProduct);
 })
