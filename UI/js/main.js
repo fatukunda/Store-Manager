675468a5799cf84809b01805b575d2ac7ce6363f
Frank <fatukunda@gmail.com>
2018-10-15 12:12:41 +0300

    <Finished: 161108617> Finished shopping cart

--- UI/js/main.js
+++ UI/js/main.js
@@ -20,6 +20,8 @@ const categorizeBtn = document.getElementsByClassName('categorize')[0];
 const categorizeRow = document.getElementsByClassName('toggle-categorize')[0];
 const quantityRow = document.getElementsByClassName('toggle-sell')[0];
 const sellProductDiv =document.getElementById('sell-product');
+const addToCartButton = document.getElementById('add-to-cart-btn');
+const cartBody = document.getElementById('cart-body');
 
 //Fill the sell product form when the sell button is clicked
 
@@ -29,19 +31,55 @@ for(i = 0; i<sellBtns.length; i++){
         let priceElement = clickedBtn.parentElement.previousElementSibling;
         let productNameElement = priceElement.previousElementSibling.previousElementSibling;
         let unitPrice = document.getElementById('unit-price');
-        let productName = document.getElementById('product')
+        let productName = document.getElementById('product');
         unitPrice.value = parseFloat(priceElement.innerText);
         productName.value = productNameElement.innerText; 
     })
 }
+addToCartButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const productTable = document.getElementById('product-table')
+    const item = document.getElementById('product').value;
+    const unitPrice = document.getElementById('unit-price').value;
+    const tableRow = document.createElement('tr');
+    const tableDataItem = document.createElement('td');
+    const tableDataPrice = document.createElement('td');
+    const tableDataQuantity = document.createElement('td');
+    const productQuantity = document.getElementById('product-quantity');
 
-for(i =0; i<sellBtns.length; i++){
-    sellBtns[i].addEventListener('click', (event) => {
-        clickedBtn = event.target;
-        hide(categorizeRow);
-        show(quantityRow);
-    });
+    //Item table data
+    tableDataItem.innerText = item
+    tableRow.appendChild(tableDataItem);
+    //Quantity table data
+    tableDataQuantity.innerText = parseInt(productQuantity.value);
+    tableRow.appendChild(tableDataQuantity);
+    //Unit price table data
+    tableDataPrice.innerText = parseFloat(unitPrice) * tableDataQuantity.innerText;
+    tableDataPrice.className += 'priceTd';
+    tableRow.appendChild(tableDataPrice);
+    // Append the product row to the table
+
+    productTable.appendChild(tableRow);
+    calculateTotal()
+});
+
+const calculateTotal = () => {
+    const numberOfItems = document.getElementsByClassName('priceTd');
+    const totalTableData = document.getElementById('total');
+    let total = 0;
+    for(let i =0; i <numberOfItems.length; i++){
+        total += parseFloat(numberOfItems[i].innerText);
+    }
+    totalTableData.innerText = 'UGX. '+ total;
 }
+
+// for(i =0; i<sellBtns.length; i++){
+//     sellBtns[i].addEventListener('click', (event) => {
+//         clickedBtn = event.target;
+//         hide(categorizeRow);
+//         show(quantityRow);
+//     });
+// }
 
 
 
