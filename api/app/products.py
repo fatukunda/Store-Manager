75994dbba108ab0dc802f6ce28f26d9a440cbcbb
Frank <fatukunda@gmail.com>
2018-10-16 11:37:31 +0300

    Finish <#161217690> GET/api/v1/products/<id>

--- api/app/products.py
+++ api/app/products.py
@@ -1,17 +1,21 @@
+import uuid
 products = [
     {
+    'id': str(uuid.uuid4()),
     'name': 'Samsung 32 inch Tv',
     'category': 'Electronics', 
     'quantity': 10,
     'price': 16000000
 },
 {
+    'id': str(uuid.uuid4()),
     'name': 'Samsung 24 inch Tv',
     'category': 'Electronics', 
     'quantity': 5,
     'price': 12000000
 },
 {
+    'id': str(uuid.uuid4()),
     'name': 'Dell laptop',
     'category': 'Computers', 
     'quantity': 7,
--- api/app/views.py
+++ api/app/views.py
@@ -7,3 +7,13 @@ from app import app
 @app.route('/api/v1/products')
 def get_products():
     return jsonify(products)
+
+# Get a single product GET/api/v1/products/<id>
+@app.route("/api/v1/products/<id>")
+def get_product(id):
+    product_list = []
+    for product in products:
+        for key in product:
+            if product[key] == id:
+                product_list.append(product)
+    return jsonify(product_list)
