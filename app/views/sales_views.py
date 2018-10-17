48374559a94ab9b0acace5195632a1d95b602474
Frank <fatukunda@gmail.com>
2018-10-17 11:06:46 +0300

    <Finish: #161277033> GET/api/v1/admin/sales/<id>

--- app/views/sales_views.py
+++ app/views/sales_views.py
@@ -10,3 +10,13 @@ bp = Blueprint('sales_views', __name__, url_prefix='/api/v1')
 @bp.route('/admin/sales')
 def get_sales():
     return jsonify(sales)
+
+# Get a single sale by admin
+@bp.route('/admin/sales/<id>')
+def admin_get_single_sale(id):
+    sale_list = []
+    for sale in sales:
+        for key in sale:
+            if sale[key] == id:
+                sale_list.append(sale)
+    return jsonify(sale_list)
