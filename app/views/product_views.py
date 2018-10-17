a2f8572ac2d9937c1415e39c572314db84791473
Frank <fatukunda@gmail.com>
2018-10-17 09:54:04 +0300

    <Finish: #161217690> GET/api/v1/products

--- app/views/product_views.py
+++ app/views/product_views.py
@@ -5,6 +5,17 @@ from app.models.products import products
 
 bp = Blueprint('product_views', __name__, url_prefix='/api/v1')
 
+# Get all products in the store
 @bp.route('/products', methods= ['GET'])
 def get_products():
     return jsonify(products)
+
+#Get a single product details
+@bp.route('/products/<id>')
+def get_single_product(id):
+    product_list = []
+    for product in products:
+        for key in product:
+            if product[key] == id:
+                product_list.append(product)
+    return jsonify(product_list)
