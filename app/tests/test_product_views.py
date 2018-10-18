e20df30a2f0ee46cbae0fbcd982597f7c3cf09ba
Frank <fatukunda@gmail.com>
2018-10-18 18:47:05 +0300

    <Start: #161307112> start testing GET/api/v1/products

--- app/tests/test_product_views.py
+++ app/tests/test_product_views.py
@@ -1,7 +1,25 @@
-from flask import request
-import unittest
+from flask import request,jsonify, json
+import pytest
 from app.models.products import products
+from app import app
 
+@pytest.fixture
+def client(request):
+    app.config['TESTING'] = True
+    client = app.test_client()
 
-class TestProductViews(unittest.TestCase):
-    pass
+    yield client
+
+def post_json(client, url, json_dict):
+    """Send dictionary json_dict as a json to the specified url """
+    return client.post(url, data=json.dumps(json_dict), content_type='application/json')
+
+def json_of_response(response):
+    """Decode json from response"""
+    return json.loads(response.data.decode('utf8'))
+
+def test_get_products_returns_all_products(client):
+    with client:
+        res = client.get('/api/v1/products')
+        assert res.status_code == 200
+        
