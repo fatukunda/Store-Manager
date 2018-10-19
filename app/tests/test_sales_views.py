294e4e81e0e444f3ceef55e685579bcdca3037b8
Frank <fatukunda@gmail.com>
2018-10-19 11:05:52 +0300

    <Finish: #161307175> Test for GET/api/v1/attendants/<username>/sales/<id>

--- app/tests/test_sales_views.py
+++ app/tests/test_sales_views.py
@@ -38,9 +38,17 @@ def test_get_single_sale_returns_a_sale(client):
 # Test GET/api/v1/attendants/<username>/sales
 def test_attendant_get_sales(client):
     with client:
-        res = client.get('api/v1/attendants/<username>/sales')
+        res = client.get('/api/v1/attendants/<username>/sales')
         assert res.status_code == 200
         assert json_of_response(res)
+
+# Test GET/api/v1/attendants/<username>/sales/<id>
+def test_get_single_sale_for_attendant(client):
+    with client:
+        res = client.get('/api/v1/attendants/<username>/sales/<id>')
+        assert res.status_code == 200
+        assert json_of_response(res)
+        assert len(json_of_response(res)) == 1
 
 
 # def test_add_product_adds_a_product(client):
