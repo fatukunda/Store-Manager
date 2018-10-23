0bf82f99e91115ff4ac957d48d29bc572755577d
Frank <fatukunda@gmail.com>
2018-10-23 15:59:43 +0300

    create run.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,8 +8,3 @@ app = Flask(__name__, instance_relative_config=True)
 app.register_blueprint(product_views.bp)
 #Register the sales_view blueprint
 app.register_blueprint(sales_views.bp)
-
-
-
-if __name__ == '__main__':
-   app.run(debug=True)
