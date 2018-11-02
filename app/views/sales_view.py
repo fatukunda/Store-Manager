57a36a9756cc31f55e8d2d9407a26411ab0e31fc
Frank <fatukunda@gmail.com>
2018-11-02 11:36:20 +0300

    finish validation

--- app/views/sales_view.py
+++ app/views/sales_view.py
@@ -22,10 +22,14 @@ def get_sales():
 def get_a_single_sale(sale_id):
         """ Get sale details"""
         current_user = get_jwt_identity()
-        if sale_controller.get_sale_details(sale_id):
-                return jsonify(sale_controller.get_sale_details(sale_id))
+        sales_person = search_sales_person(current_user)
+        if sales_person['user_type'] == 'admin':
+                if sale_controller.get_sale_details(sale_id):
+                        return jsonify(sale_controller.get_sale_details(sale_id))
+                else:
+                        return jsonify({"message": "Sale Item with an Id of {} was not found".format(sale_id)}), 404
         else:
-                return jsonify({"message": "Sale Item with an Id of {} was not found".format(sale_id)}), 404
+                return jsonify({"message": "Not authorized to access this route"})
 
 @bp.route('attendants/<attendant_id>/sales')
 @jwt_required
@@ -33,7 +37,6 @@ def get_attendant_sales(attendant_id):
         # Get all sales made by a particular store attendant
 
         return jsonify(sale_controller.get_all_sales(attendant_id))
-
 
 @bp.route('/sales', methods=['POST'])
 @jwt_required
