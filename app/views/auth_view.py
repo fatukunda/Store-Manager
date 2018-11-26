256d9e34473e6e1fa93e84a31fd319f2a7d86fe9
Frank Atukunda <fatukunda@hotmail.com>
2018-11-26 13:06:08 +0300

    Update auth_view.py

--- app/views/auth_view.py
+++ app/views/auth_view.py
@@ -28,8 +28,9 @@ def login():
     access_token = ''
     user_id = user['user_id']
     user_type = user['user_type']
+    username = user['username']
     access_token = create_access_token(identity= user_id, expires_delta= timedelta(hours=2))
-    return jsonify(access_token=access_token, user_type = user_type), 201
+    return jsonify(access_token=access_token, user_type = user_type, username = username), 201
 
 
     
